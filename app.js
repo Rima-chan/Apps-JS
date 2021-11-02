e585ac8a71252ee08f64bcad3a301fa5f5cfe925
Marie Beaujeu <marie.beaujeu@gmail.com>
2021-11-02 20:41:00 +0100

    add sticky note and recipe app

--- app.js
+++ app.js
@@ -3,13 +3,25 @@ const data = [
         id: "app1",
         title: "Météo app",
         img: "./images/app_meteo_capture.png",
-        link: "https://rima-chan.github.io/Meteo-app/"
+        link: "https://rima-chan.github.io/Meteo-app-JS/"
     },
     {
         id: "app2",
         title: "To-do Liste",
-        img: "./images/todoListe_capture.png",
+        img: "./images/todoListe_screen.png",
         link: "https://rima-chan.github.io/todo-list-JS/"
+    },
+    {
+        id:"app3",
+        title: "Sticky Notes",
+        img: "./images/sticky_note_capture.png",
+        link: "https://rima-chan.github.io/sticky-notes/" 
+    },
+    {
+        id:"app4",
+        title: "Recette App (EN)",
+        img: "./images/recette_app_capture.png",
+        link: "https://rima-chan.github.io/Recipe-app-JS/index.html" 
     },
 
 ]
@@ -28,7 +40,7 @@ function displayCards(data) {
                                 >
                                     <div class="relative">
                                         <img src="./images//js-logo.png" class="w-16 h-16 absolute top-0 left-0 z-40">
-                                        <img src="${item.img}">
+                                        <img src="${item.img}" class="w-full object-coverè">
                                     </div>
                                     <div class="shadow-inner ">
                                         <h2 class="uppercase font-semibold text-center py-8">${item.title}</h2>
